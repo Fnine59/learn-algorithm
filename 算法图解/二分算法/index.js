72bc442a5da8b03cec59afe0d96d0b02a21807b9
Fnine59 <fnine59@163.com>
2019-11-06 16:02:44 +0800

    二分算法 100%

--- 算法图解/二分算法/index.js
+++ 算法图解/二分算法/index.js
@@ -9,13 +9,41 @@ function getOrderedArray(range = [1, 10]) {
 // 数据准备工具方法 end
 
 // 初始数据 start
-const range = [1, 10];
+const range = [1, 16];
 const arr = getOrderedArray(range);
-const target = 8;
+const target = 16;
 // 初始数据 end
 
 // 二分算法 start
-function binary() {
-  
+/**
+ * @param {Number} target 要查找的目标数字
+ * @param {Array} arr 要查找的数组
+ */
+function binary(target, arr) {
+  let low = 0, // 左侧指针
+    high = arr.length - 1, // 右侧指针
+    count = 0,
+    index = -1; // 查找次数
+  while (low <= high) {
+    count++;
+    const i = low + Math.floor((high - low) / 2);
+    if (arr[i] === target) {
+      index = i;
+      break;
+    }
+    if (arr[i] > target) {
+      high = i - 1;
+    }
+    if (arr[i] < target) {
+      low = i + 1;
+    }
+  }
+  return {
+    count,
+    index,
+  }
 }
+console.log(`要查找的元素是${target}，查找的数组是${arr}`);
+const { count, index } = binary(target, arr);
+console.log(`二分查找到元素的位置是：${index}，查找次数是${count}。`);
 // 二分算法 end
